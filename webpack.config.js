1ce1740cc53f155c665b73cfd0bb547519258b3f
Ashish Singh <ashishcodes4@gmail.com>
2018-12-30 04:07:54 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  //This property defines where the application starts
+  entry:'./src/index.js',
+  //This property defines the file path and the file name which will be used for deploying the bundled file
+  output:{
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+  //Setup loaders
+  module: {
+    rules: [
+      {
+        test: /\.js$/, 
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
