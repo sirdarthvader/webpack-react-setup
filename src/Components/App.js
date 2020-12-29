460bc65e73782c7c62ba3a2a31a9a8f60f701061
Ashish Singh <ashishcodes4@gmail.com>
2020-12-30 05:02:22 +0530

    Fix security vulnerability
    * Updated webpack, webpack-cli and few plugins
    * Made changes in webpack config file to match with the updated "HtmlWebpackPlugin"
    * Changed class component to functional component
    * Added detailed documentation for additions made in webpack config
    
    [Ticket: N.A.]

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react'
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Webpack + React setup!!!</h1>
-      </div>
-    )
-  }
+export default function App() {
+  return (
+    <div>
+      <h1>Webpack + React setup!!!</h1>
+    </div>
+  );
 }
-
-export default App;
