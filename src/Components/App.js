14e8005bfdba25d47e03b004f97de53d0c47c29f
Ashish Singh <ashishcodes4@gmail.com>
2021-05-05 12:32:06 +0530

    Fixed issues with webpack
    Changed a bunch of files almost the whole repo, to remove the mistakes of past and start afresh.

--- src/Components/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function App() {
-  return (
-    <div>
-      <h1>Webpack + React setup!!!</h1>
-    </div>
-  );
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,28 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  //This property defines starting point of application (main entry)
-  entry: "./src/index.js",
-  //This property defines the file path and the file name which will be used for deploying the final production bundle
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "index_bundle.js",
-  },
-  //Setup loaders
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Custom template",
-      // Load a custom template (lodash by default)
-      template: "./src/index.html",
-      meta: {
-        viewport: "width=device-width, initial-scale=1, shrink-to-fit=no",
-        // Will generate: <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        "theme-color": "#4285f4",
-        // Will generate: <meta name="theme-color" content="#4285f4">
-      },
-    }),
-  ],
-};
+   mode: "development", // change this to production before doing the final build
+   entry: "./index.js",
+   output: {
+      path: path.resolve( __dirname, "public" ),
+      filename: "main.js"
+   },
+   target: "node",
+   devServer: {
+      port: "8888",
+      contentBase: ["./public"],
+      open: true
+   },
+   resolve: {
+      extensions: [".js", ". jsx", ". json"]
+   },
+   module: {
+      rules: [{
+         test: /\.(js|jsx)$/,
+         exclude: / node_modules /,
+         use: {
+            loader: "babel-loader"
+         }
+      }]
+   }
+}
